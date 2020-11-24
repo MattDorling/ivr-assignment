84a6b37aad2d4036a3ae996054214b0e74e3c2c3
MattDorling <dorling.mj@gmail.com>
2020-11-24 13:18:20 -0400

    chooses best circles but sometimes drops them

--- src/image1_4.3.py
+++ src/image1_4.3.py
@@ -45,11 +45,13 @@ class image1_43_converter:
         # initialize arrays to store joint positions
         self.joint1_pos = np.array([])
         self.joint2_pos = np.array([])
-        self.joint3_pos = np.array([])
-        self.joint4_pos = np.array([])
+        self.joint3_pos = np.array([400,380])
+        self.joint4_pos = np.array([400,310])
 
         # record start time
         self.time_trajectory = rospy.get_time()
+
+        self.first_callback = True
 
   # receive data from camera 1, process it, and publish
     def callback1(self,data):
@@ -68,6 +70,8 @@ class image1_43_converter:
         im1=cv2.imshow('window1', self.cv_image1)
         cv2.waitKey(1)
 
+        self.first_callback = False
+
         # publish the results
         try: 
             self.image_pub1.publish(self.bridge.cv2_to_imgmsg(self.cv_image1, "bgr8"))
@@ -76,7 +80,7 @@ class image1_43_converter:
 
     # find the centres of the joints and publishes these to topics
     def find_joints(self, image):
-        im=image
+        im=image.copy()
         gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         ret, thresh = cv2.threshold(gray, 15, 255, cv2.THRESH_BINARY_INV)
         # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
@@ -85,7 +89,7 @@ class image1_43_converter:
         # cv2.imshow('contours',im_edge)
         # cv2.waitKey(1)
 
-        circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, im.shape[0]/64, param1=200, param2=10, minRadius=5, maxRadius=30)
+        circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 1, im.shape[0]/64, param1=200, param2=6, minRadius=1, maxRadius=11)
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0, :]:
@@ -93,35 +97,54 @@ class image1_43_converter:
                 cv2.circle(im, (i[0], i[1]), i[2], (0, 255, 0), 2)
                 # Draw inner circle
                 cv2.circle(im, (i[0], i[1]), 2, (0, 0, 255), 3)
-            cv2.imshow('circles',circles)
-            cv2.waitKey(1)
         # im_edge_all = cv2.Canny(gray,100,200,apertureSize=5,L2gradient=True)
         # im_and = cv2.bitwise_not(im_edge_gray)
-        cv2.imshow('and',thresh)
+
+
+        centres = []
+        if circles is not None:
+            for c in circles[0,:]:
+                centres.append((c[0],c[1]))
+
+        joint4_candidate = self.find_nearest(centres,self.joint4_pos)
+        if joint4_candidate is not None:
+            self.joint4_pos = joint4_candidate
+            centres.remove(joint4_candidate)
+
+        joint3_candidate = self.find_nearest(centres,self.joint3_pos)
+        if joint3_candidate is not None:
+            self.joint3_pos = joint3_candidate
+        
+        # cv2.drawMarker(im, self.joint3_pos,(0,255,0))
+        cv2.drawMarker(im, (int(self.joint3_pos[0]),int(self.joint3_pos[1])),(0,255,0))
+        cv2.drawMarker(im, (int(self.joint4_pos[0]),int(self.joint4_pos[1])),(255,0,0))
+
+        cv2.imshow('im',im)
         cv2.waitKey(1)
 
-        # set up a blob detector to detect circles
-        # p = cv2.SimpleBlobDetector_Params()
-        # p.filterByCircularity = True
-        # p.minCircularity = 0.2
-        # p.filterByConvexity = False
-        # p.filterByArea = False
-        # p.filterByInertia = False
-        # blob_det = cv2.SimpleBlobDetector_create(p)
-        # keypoints = blob_det.detect(thresh)
+        a = Float64MultiArray()
+        a.data = self.joint3_pos
+        self.joint3_est_pub.publish(a)
+        a.data = self.joint4_pos
+        self.joint4_est_pub.publish(a)
 
-        # # there should be one circle so one keypoint only.
-        # if len(keypoints) > 0:
-        #     h, w = 50, 50
-        #     cx,cy = keypoints[0].pt
-        #     sub_img = image[int(cy-h/2):int(cy+h/2), int(cx-w/2):int(cx+w/2)]
-        #     M = cv2.moments(sub_img)
-        #     cx = int(cx - w/2 + M["m10"] / M["m00"])
-        #     cy = int(cy - h/2 + M["m01"] / M["m00"])
-        #     blobs = cv2.drawMarker(image, (int(cx),int(cy)), (0,0,255))
-        #     number_of_blobs = len(keypoints) 
-        #     cv2.imshow("Filtering Circular Blobs Only", blobs) 
+    def find_nearest(self, centres, prev):
+        if len(centres) == 0:
+            return None
+        closest = centres[0]
+        for c in centres:
+            if self.distance(c,prev) < self.distance(closest,prev):
+                closest = c
+        if self.distance(closest,prev) > 20: #TODO check this constant
+            return None
+        else:
+            return closest
 
+
+    def distance(self, pos_1, pos_2):
+        pos_1 = np.array([pos_1[0],pos_1[1]])
+        pos_2 = np.array([pos_2[0],pos_2[1]])
+        return np.sqrt(np.sum((pos_1 - pos_2)**2))
 
     # detecting the centre of the {colour} circle
     def detect_colour(self, joint, image, bgr_low, bgr_up):
@@ -140,14 +163,14 @@ class image1_43_converter:
     def move_joints(self):
         t = rospy.get_time() - self.time_trajectory
         # ~~comment out the following 3 lines for the robot to run indefinitely~~
-        if (t >= 5):    # reset time to 0 after 5 seconds
-            t = 0
-            self.time_trajectory = rospy.get_time()
+        # if (t >= 5):    # reset time to 0 after 5 seconds
+            # t = 0
+            # self.time_trajectory = rospy.get_time()
 
         # calculate sinusoidal signals
         j2 = (np.pi / 2) * np.sin((np.pi/15) * t)
         j3 = (np.pi / 2) * np.sin((np.pi/18) * t)
-        j4 = (np.pi / 2) * np.sin((np.pi/20) * t)
+        j4 = (np.pi / 3) * np.sin((np.pi/20) * t)
         # ~~change j4 pi/2 to pi/3 to prevent EE hitting ground if running longer than 5 seconds~~
 
         # publish the results
@@ -167,3 +190,4 @@ def main(args):
 # run the code if the node is called
 if __name__ == '__main__':
     main(sys.argv)
+
